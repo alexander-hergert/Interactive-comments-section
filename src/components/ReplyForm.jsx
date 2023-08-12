242317d85feade1f3c707eb7a0c99130d5ad4532
Alexander Hergert <alexander.hergert1989@gmail.com>
2023-08-12 13:45:31 +0200

    Fix Reply bug for quotes on fetch function.

--- src/components/ReplyForm.jsx
+++ src/components/ReplyForm.jsx
@@ -81,6 +81,7 @@ const ReplyForm = ({ commentId, handleToggle, replyingTo }) => {
     const response = await fetch("https://api.adviceslip.com/advice");
     const data = await response.json();
     setTextContent(data.slip.advice);
+    return data.slip.advice;
   };
 
   useEffect(() => {
